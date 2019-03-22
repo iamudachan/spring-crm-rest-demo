c8efd98e709f23635a3f86ec35ff5b35342b5c64
iamudachan <47654533+iamudachan@users.noreply.github.com>
2019-03-22 16:39:09 +0530

    spring rest with CURD operations

---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CutomerNotFoundException.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo.rest;
+
+public class CutomerNotFoundException extends Exception {
+
+	public CutomerNotFoundException() {
+	}
+
+	public CutomerNotFoundException(String arg0) {
+		super(arg0);
+	}
+
+	public CutomerNotFoundException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public CutomerNotFoundException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public CutomerNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
